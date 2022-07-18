bf7e6604c06edcfa1b3e01e358027a8863ae3dc4
Oom Prakash <85257571+oomprakash99@users.noreply.github.com>
2022-07-18 09:18:45 +0530

    added

---
+++ src/app/dash/dash.component.ts
@@ -0,0 +1,56 @@
+import { Component, HostListener, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dash',
+  templateUrl: './dash.component.html',
+  styleUrls: ['./dash.component.css']
+})
+export class DashComponent implements OnInit {
+
+  constructor() { }
+  
+  error!: string;
+  dragAreaClass!: string;
+  onFileChange(event: any) {
+    let files: FileList = event.target.files;
+    this.saveFiles(files);
+  }
+  ngOnInit() {
+    this.dragAreaClass = "dragarea";
+  }
+  @HostListener("dragover", ["$event"]) onDragOver(event: any) {
+    this.dragAreaClass = "droparea";
+    event.preventDefault();
+  }
+  @HostListener("dragenter", ["$event"]) onDragEnter(event: any) {
+    this.dragAreaClass = "droparea";
+    event.preventDefault();
+  }
+  @HostListener("dragend", ["$event"]) onDragEnd(event: any) {
+    this.dragAreaClass = "dragarea";
+    event.preventDefault();
+  }
+  @HostListener("dragleave", ["$event"]) onDragLeave(event: any) {
+    this.dragAreaClass = "dragarea";
+    event.preventDefault();
+  }
+  @HostListener("drop", ["$event"]) onDrop(event: any) {
+    this.dragAreaClass = "dragarea";
+    event.preventDefault();
+    event.stopPropagation();
+    if (event.dataTransfer.files) {
+      let files: FileList = event.dataTransfer.files;
+      this.saveFiles(files);
+    }
+  }
+
+  saveFiles(files: FileList) {
+
+    if (files.length > 5) this.error = "Only five files at time allowed";
+    else {
+      this.error = "";
+      // console.log(files[].size,files[].name,files[].type);
+      console.log(files);
+    }
+  }
+}
